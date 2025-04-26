671a1524a1bbaa9328c12239c5081d808050a2bf
OutisLi <LTC201806070316@gmail.com>
2025-04-26 15:17:03 +0800

    20250426-dp_init machine

--- example/2_abacus-dpgen/job_dp_init.sh
+++ example/2_abacus-dpgen/job_dp_init.sh
@@ -1,11 +1,31 @@
-#!/bin/bash
-#SBATCH -o slurm.%j.out
+#!/usr/bin/env bash
+#SBATCH -o %j.out
+#SBATCH -p C064M0256G
 #SBATCH --qos=low
-#SBATCH -] dpinit
+#SBATCH -J dp_init_3C
 #SBATCH --nodes=1
 #SBATCH --ntasks=64
-#SBATCH -t 120:0:0
+#SBATCH -t 120:00:00
 #SBATCH --account hpc0006168316
-#SBATCH--partition C064M0256G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=LTC201806070316@outlook.com
+
+# 获取开始时间戳
+start_time=$(date +%s)
 
 dpgen init_bulk /lustre/home/2201210084/abacus_learn/example/2_abacus-dpgen/init/3C/param.json /lustre/home/2201210084/abacus_learn/example/2_abacus-dpgen/init/3C/machine_wm.json
+
+# 获取结束时间戳
+end_time=$(date +%s)
+
+# 计算时间差
+elapsed_time=$((end_time - start_time))
+
+# 转换为小时、分钟、秒
+hours=$((elapsed_time / 3600))
+minutes=$(( (elapsed_time % 3600) / 60 ))
+seconds=$((elapsed_time % 60))
+
+# 输出执行时间
+echo "Running dp_init_3C Successfully"
+echo "Execution time: ${hours}h ${minutes}m ${seconds}s"
